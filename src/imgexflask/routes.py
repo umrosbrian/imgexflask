e0962a3c7363c92dba12eeaa76f7ded2ff0d6ac2
rosbrian <rosbrian@umich.edu>
2022-12-24 20:43:26 -0500

    added logging

--- src/imgexflask/routes.py
+++ src/imgexflask/routes.py
@@ -1,36 +1,37 @@
+import logging
 from imgexflask import app
 from flask import render_template, request, session
 from flask_flatpages import FlatPages
+from werkzeug.security import check_password_hash
+import datetime as dt
+import os
 
 
-# Some configuration, ensures
-# 1. Pages are loaded on request.
-# DEBUG = True
-# FLATPAGES_AUTO_RELOAD = DEBUG
+current_time = dt.datetime.now().strftime('%Y%m%d_%H%M%S')
+log_file_aps = os.path.join(os.path.dirname(os.path.dirname(app.instance_path)), 'logs', f"{current_time}.log")
+logging.basicConfig(filename=log_file_aps,
+                    filemode='w',
+                    level=logging.DEBUG,
+                    format='%(levelname)s-%(asctime)s-%(funcName)s - %(message)s')
+
+# Files having this extension will be the only ones that are returned from the 'pages' directory.
 FLATPAGES_EXTENSION = '.php'
+# This is relative to the app root, which is within the package directory.
+FLATPAGES_ROOT = os.path.join(os.pardir, os.pardir, os.pardir, 'pages')
 
 # This is somehow setting the path to the 'pages' directory.  Don't see anything in app.config that's changed.
 app.config.from_object(__name__)
 pages = FlatPages(app)
 
 # I think this is needed for request...it's not just a WTF thing
-app.config["SECRET_KEY"] = "ITSASECRET"
-
-
-# URL Routing - Flat Pages
-# Retrieves the page path and
-@app.route('/<path:path>')
-def page(path):
-    if session['logged_in']:
-        page = pages.get_or_404(path)
-        return render_template("page.html", page=page)
-    else:
-        return home()
+app.config["SECRET_KEY"] = 'e^Px\9]D@g*"*`^:+4+T'
 
 
 @app.route('/')
+@app.route('/index')
 def home():
-    if not session['logged_in']:
+    # Don't know why I can't use session['logged_in'].
+    if not session.get('logged_in'):
         return render_template('login.html')
     else:
         # Articles are pages with a publication date
@@ -45,13 +46,49 @@ def home():
         print (latest)
         return render_template('index.html', articles=latest[:10])
 
+
+# I found that if I put in the URL for index after logging out the template would be rendered.  This prevents that \
+# behavior
+@app.route('/login')
+def login_get():
+    if session['logged_in']:
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
+
+
+# werkzeug is installed as a dependency of flask.  You can use `from werkzeug.security import generate_password_hash` \
+# then print(generate_password_hash(<password>, 'sha256')) to generate the hash of <password>.  You can then hardcode \
+# this hashed password and test to see if the provided password's hash matches the hardcoded hash.
+# https://pythonspot.com/login-authentication-with-flask/ for this method of the form & session method of authentication
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['username'] == 'admin' and request.form['password'] == 'p':
+    provided_uname = request.form['username']
+    provided_pword = request.form['password']
+    hashed_pword = 'sha256$qy5yj2dnBfKzDSzy$97b4acbbd4d593bb299efbc73e2c5a1c0a8382693a243b868f2466ca817067e3'
+    # The hash of the provided password when correct will be different every time so we can't use a simple string \
+    # comparison.  We need to use check_password_hash().
+    if provided_uname == 'admin' and check_password_hash(hashed_pword, provided_pword):
         session['logged_in'] = True
-    return home()
+        logging.debug(f"{provided_uname} logged in")
+    else:
+        logging.warning(f"User {provided_uname} attempted to log in with password {provided_pword}.")
+    return render_template('index.html')
+
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return home()
+    logging.debug(f"logged out")
+    return render_template('login.html')
+
+
+# URL Routing - Flat Pages
+# Retrieves the page path and
+@app.route('/<path:path>')
+def page(path):
+    if session['logged_in']:
+        page = pages.get_or_404(path)
+        return render_template("page.html", page=page)
+    else:
+        return render_template('login.html')
