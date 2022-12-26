e9c50af711f7065de50751875441638adf661d15
rosbrian <rosbrian@umich.edu>
2022-12-25 20:57:09 -0500

    changes flatpages_root

--- src/imgexflask/routes.py
+++ src/imgexflask/routes.py
@@ -18,7 +18,7 @@ logging.basicConfig(filename=log_file_aps,
 # Files having this extension will be the only ones that are returned from the 'pages' directory.
 FLATPAGES_EXTENSION = '.php'
 # Directory containing the files that will be rendered with page().
-FLATPAGES_ROOT = os.path.join(os.path.dirname(app.root_path), 'pages')
+FLATPAGES_ROOT = os.path.join(os.path.dirname(os.path.dirname(app.root_path)), 'pages')
 logging.debug(f"FLATPAGES_ROOT: {FLATPAGES_ROOT}")
 
 # This is somehow setting the path to the 'pages' directory.  Don't see anything in app.config that's changed.
